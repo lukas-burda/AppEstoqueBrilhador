3756657a31ebf34e2d27e99c4df14a3fd51bf8f2
Lukas Ferreira <contatolukasferreira@gmail.com>
2020-05-15 02:42:50 -0300

    commit 14-05-2020

---
+++ AppDAL/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using AppDomain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppDAL
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AppEstoqueBrilhador;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ AppDomain/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppDomain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double Amount { get { return Quantity * Price; } }
+        public int ProviderId { get; set; }
+    }
+}
