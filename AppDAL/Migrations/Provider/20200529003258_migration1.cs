37d39b6def0d2a205854f734c8060eef7f07eabf
Luiz Henrique <luiz.valesko@gmail.com>
2020-06-02 22:25:40 -0300

    Correção

---
+++ AppDAL/Migrations/Provider/20200529003258_migration1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppDAL.Migrations.Provider
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
